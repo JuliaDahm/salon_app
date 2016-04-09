94f05b219ca6408bda9c50681fe44660348d4997
Brandon Huynh <brandonvhuynh@gmail.com>
2016-04-09 12:24:02 -0600

    fixed signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-        sessions: 'users/registrations'
+        sessions: 'users/sessions'
       }
   root 'products#index'
   resources :customers
